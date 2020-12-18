2eb795355b532253a2b985a71dc93ad9ffbce6bc
Kirill <ke.malikov@yandex.ru>
2020-12-18 16:23:49 +0400

    MAIN

--- gulpfile.js
+++ gulpfile.js
@@ -134,29 +134,6 @@ gulp.task('otf2ttf', function(){
     .pipe(dest(source_folder + '/fonts/'));
 })
 
-// function fontsStyle(params) {
-
-//   let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
-//   if (file_content == '') {
-//   fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
-//   return fs.readdir(path.build.fonts, function (err, items) {
-//   if (items) {
-//   let c_fontname;
-//   for (var i = 0; i < items.length; i++) {
-//   let fontname = items[i].split('.');
-//   fontname = fontname[0];
-//   if (c_fontname != fontname) {
-//   fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-//   }
-//   c_fontname = fontname;
-//   }
-//   }
-//   })
-//   }
-//   }
-  
-//   function cb() { }
-
 function watchFiles(params){
     gulp.watch([path.watch.html],html);
     gulp.watch([path.watch.css],css);
@@ -168,10 +145,9 @@ function clean(params){
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
-exports.fontsStyle = fontsStyle;
 exports.fonts = fonts;
 exports.js = js;
 exports.images = images;
